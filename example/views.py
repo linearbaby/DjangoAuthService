3286bd32fc4e12d4d305b007145a61144ef63513
root <root@DOMIC>
2024-05-23 02:50:36 +0300

    Added group functionality

--- example/views.py
+++ example/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import Group
 
 class CheckAuthView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
-        return Response(True)
+        return JsonResponse({
+            "detail": "authenticated",
+            "pro_version": request.user.groups.filter(name='CanUseChatGPT').exists()
+        })
